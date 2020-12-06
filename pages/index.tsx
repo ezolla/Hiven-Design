1f1ff82e55287c9f33b107e3db27445a6f8294d3
ezolla <ezoller11@gmail.com>
2020-12-05 21:04:53 -0800

    Added Typescript support

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hiven Design</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>Hiven Design</h1>
+      </main>
+    </div>
+  )
+}
