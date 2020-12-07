c8f92f1296862938ba71cd48dcbd345583df87ca
ezolla <ezoller11@gmail.com>
2020-12-07 14:43:25 -0800

    Added color swatches to design cards

--- components/DesignStyle.tsx
+++ components/DesignStyle.tsx
@@ -12,17 +12,41 @@ export default function DesignStyle(props: Props) {
   const [value, setValue] = useState(props.code)
   const [copied, setCopied] = useState(false)
 
+  const decoded = atob(props.code)
+  console.log(decoded)
+
+  const format = decoded.split(',')
+  console.log(format)
+
+  let clean = format.splice(1)
+  clean = clean.splice(0, 13)
+  console.log(clean.length)
+
   return (
     <DesignStyleCard>
-      {/* Title */}
-      <Title>{props.name}</Title>
+      <HeadingWrap>
+        {/* Title */}
+        <Title>{props.name}</Title>
 
-      {/* Copy Button */}
-      <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
-        <Copy>
-          <Image src='/clipboard.svg' alt='Clipboard' width={18} height={18} />
-        </Copy>
-      </CopyToClipboard>
+        {/* Copy Button */}
+        <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
+          <Copy>
+            <Image
+              src='/clipboard.svg'
+              alt='Clipboard'
+              width={18}
+              height={18}
+            />
+          </Copy>
+        </CopyToClipboard>
+      </HeadingWrap>
+
+      <ColorWrap>
+        {/* Color Swatches */}
+        {clean.map((color) => {
+          return <ColorSwatch color={color} />
+        })}
+      </ColorWrap>
     </DesignStyleCard>
   )
 }
@@ -30,10 +54,13 @@ export default function DesignStyle(props: Props) {
 const DesignStyleCard = styled.div`
   background: var(--color-dark);
   border-radius: 2px;
+  padding: 10px;
+`
+
+const HeadingWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px;
 `
 
 const Title = styled.h2`
@@ -48,4 +75,17 @@ const Copy = styled.a`
   background: var(--color-grey);
   border-radius: 5px;
   cursor: pointer;
+`
+
+const ColorWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`
+
+const ColorSwatch = styled.div<{ color: string }>`
+  background: ${({ color }) => color};
+  height: 8px;
+  width: 100%;
 `
